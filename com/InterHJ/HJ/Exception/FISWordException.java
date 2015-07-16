c179777d5ef8b5f452733d43af8bfe34eb864ef3
Stefano Tallarino <stefano.tallarino@gmail.com>
2015-07-16 11:29:12 +0200

    Create FISWordException.java

---
+++ com/InterHJ/HJ/Exception/FISWordException.java
@@ -0,0 +1,12 @@
+package com.InterHJ.HJ.Exception;
+
+public class FISWordException extends Exception{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	public FISWordException() { super(); }
+	public FISWordException(String message) { super(message); }
+	public FISWordException(String message, Throwable cause) { super(message, cause); }
+	public FISWordException(Throwable cause) { super(cause); }
+}
